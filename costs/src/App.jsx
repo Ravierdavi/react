5b09731bf0c8d7de0c4244a027028538b521e89d
Ravier Davi <ravierdavi1@outlook.com>
2024-10-11 15:50:39 -0300

    feat: Adiciona uma estrutura inicial para o projeto

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -1,6 +1,27 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import Home from "./components/pages/Home"
+import Projects from "./components/pages/Projects"
+import About from "./components/pages/About"
+import Contact from "./components/pages/Contact"
+
+import Layout from "./components/layout/Layout"
+
 function App() {
-  return(
-    <h1>Costs</h1>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/home"/>}/>
+          <Route path="/home" element={<Home />}></Route>
+
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+
+          <Route path="/newproject"></Route>
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ costs/src/components/layout/NavBar.jsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import styles from "./NavBar.module.css"
+
+import logo from "../../img/costs_logo.png"
+
+function NavBar() {
+    return (
+        <nav className={styles.navbar}>
+            <Link>
+                <img src={logo} alt="costs_logo" />
+            </Link>
+            <ul className={styles.list}>
+                <li className={styles.item}><Link to="/home">Home</Link></li>
+                <li className={styles.item}><Link to="/projects">Projects</Link></li>
+                <li className={styles.item}><Link to="/about">About</Link></li>
+                <li className={styles.item}><Link to="/contact">Contact</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
